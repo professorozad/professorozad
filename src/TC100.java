71ff8a6a0542ffd96cc490d623e87efb05998bb8
professorozad <113231730+professorozad@users.noreply.github.com>
2022-10-09 14:43:51 +0200

    created new Java class and TC100.java

---
+++ src/TC100.java
@@ -0,0 +1,10 @@
+public class TC100 {
+    public static TC100 newInstance() {
+
+        //var args = new Bundle();
+
+       // TC100 fragment = new TC100();
+       // fragment.setArguments(args);
+        //return fragment;
+    }
+}
